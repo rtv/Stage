031acd188055b4b9fa3c50c000b42372ebfa5c93
ahoward <ahoward@55cc97c4-4627-0410-81d5-de40420c0091>
2000-12-01 02:25:04 +0000

    Created

---
+++ include/visiondevice.hh
@@ -0,0 +1,58 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// File: visiondevice.hh
+// Author: Andrew Howard
+// Date: 30 Nov 2000
+// Desc: Simulates the ACTS vision system
+//
+// CVS info:
+//  $Source: /home/tcollett/stagecvs/playerstage-cvs/code/stage/include/visiondevice.hh,v $
+//  $Author: ahoward $
+//  $Revision: 1.1 $
+//
+// Usage:
+//  (empty)
+//
+// Theory of operation:
+//  (empty)
+//
+// Known bugs:
+//  (empty)
+//
+// Possible enhancements:
+//  (empty)
+//
+///////////////////////////////////////////////////////////////////////////
+
+#ifndef VISIONDEVICE_HH
+#define VISIONDEVICE_HH
+
+#include "playerdevice.hh"
+
+class CVisionDevice : public CPlayerDevice
+{
+    // Default constructor
+    //
+    public: CVisionDevice(void *buffer, size_t data_len, size_t command_len, size_t config_len);
+    
+    // Update the device
+    //
+    public: virtual bool Update();
+
+    // Timing properties
+    //
+    public: double m_last_update, m_update_interval;
+
+    // Camera properties
+    //
+    private: double cameraAngleMax, cameraAngleMin, cameraFOV;
+    private: double cameraPanMin, cameraPanMax, cameraPan;
+    private: int cameraImageWidth, cameraImageHeight;
+
+    private: int numBlobs;
+    private: ColorBlob blobs[MAXBLOBS];
+
+    private: unsigned char actsBuf[ACTS_TOTAL_MAX_SIZE];
+};
+
+#endif
