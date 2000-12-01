abd1f80e40a6a45bbed57e1518ec2eb7d5d5c3cd
ahoward <ahoward@55cc97c4-4627-0410-81d5-de40420c0091>
2000-12-01 21:25:32 +0000

    Added player device

---
+++ include/playerdevice.hh
@@ -0,0 +1,105 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// File: playerdevice.hh
+// Author: Andrew Howard
+// Date: 28 Nov 2000
+// Desc: Base class for all devices
+//
+// CVS info:
+//  $Source: /home/tcollett/stagecvs/playerstage-cvs/code/stage/include/playerdevice.hh,v $
+//  $Author: ahoward $
+//  $Revision: 1.1 $
+//
+// Usage:
+//  (empty)
+//
+// Theory of operation:
+//  (empty)
+//
+// Known bugs:
+//  (empty)
+//
+// Possible enhancements:
+//  (empty)
+//
+///////////////////////////////////////////////////////////////////////////
+
+#ifndef PLAYERDEVICE_HH
+#define PLAYERDEVICE_HH
+
+// For size_t
+//
+#include <stddef.h>
+
+// For base class
+//
+#include "device.hh"
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Base class for all player devices
+//
+class CPlayerDevice : public CDevice
+{
+    // Minimal constructor
+    // This is an abstract class and *cannot* be instantiated directly
+    // buffer points to a single buffer containing the data, command and configuration buffers.
+    //
+    protected: CPlayerDevice(CRobot *robot, void *buffer, size_t data_len,
+                             size_t command_len, size_t config_len);
+        
+    // Initialise the device
+    //
+    public: virtual bool Startup();
+
+    // Close the device
+    //
+    public: virtual bool Shutdown();
+
+    // Update the device
+    // This is pure virtual and *must* be overloaded
+    //
+    public: virtual bool Update() = 0;
+
+    // See if the device is subscribed
+    //
+    public: bool IsSubscribed();
+    
+    // Write to the data buffer
+    // Returns the number of bytes copied
+    //
+    protected: size_t PutData(void *data, size_t len);
+
+    // Read from the command buffer
+    // Returns the number of bytes copied
+    //
+    protected: size_t GetCommand(void *data, size_t len);
+
+    // Read from the configuration buffer
+    // Returns the number of bytes copied
+    //
+    protected: size_t GetConfig(void *data, size_t len);
+
+    // Pointer to shared info buffers
+    //
+    private: BYTE *m_info_buffer;
+    private: size_t m_info_len;
+
+    // Pointer to shared data buffers
+    //
+    private: void *m_data_buffer;
+    private: size_t m_data_len;
+
+    // Pointer to shared command buffers
+    //
+    private: void *m_command_buffer;
+    private: size_t m_command_len;
+
+    // Pointer to shared config buffers
+    //
+    private: void *m_config_buffer;
+    private: size_t m_config_len;
+};
+
+
+#endif
---
+++ src/playerdevice.cc
@@ -0,0 +1,175 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// File: playerdevice.cc
+// Author: Andrew Howard
+// Date: 28 Nov 2000
+// Desc: Base class for all player devices
+//
+// CVS info:
+//  $Source: /home/tcollett/stagecvs/playerstage-cvs/code/stage/src/playerdevice.cc,v $
+//  $Author: ahoward $
+//  $Revision: 1.1 $
+//
+// Usage:
+//  (empty)
+//
+// Theory of operation:
+//  (empty)
+//
+// Known bugs:
+//  (empty)
+//
+// Possible enhancements:
+//  (empty)
+//
+///////////////////////////////////////////////////////////////////////////
+
+#include "world.h"
+#include "playerdevice.hh"
+
+
+///////////////////////////////////////////////////////////////////////////
+// Minimal constructor
+//
+CPlayerDevice::CPlayerDevice(CRobot *robot, void *buffer, size_t data_len, size_t command_len, size_t config_len)
+        : CDevice(robot)
+{
+    m_info_buffer = (BYTE*) buffer;
+    m_info_len = INFO_BUFFER_SIZE;
+    
+    m_data_buffer = (BYTE*) m_info_buffer + m_info_len;
+    m_data_len = data_len;
+
+    m_command_buffer = (BYTE*) m_data_buffer + data_len;
+    m_command_len = command_len;
+
+    m_config_buffer = (BYTE*) m_command_buffer + command_len;
+    m_config_len = config_len;
+
+    TRACE4("creating player device at addr: %p %p %p %p", m_info_buffer, m_data_buffer,
+         m_command_buffer, m_config_buffer);
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+// Default startup -- doesnt do much
+//
+bool CPlayerDevice::Startup()
+{
+    if (!CDevice::Startup())
+        return false;   
+    return true;
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+// Default shutdown -- doesnt do much
+//
+bool CPlayerDevice::Shutdown()
+{
+    return true;
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+// See if the PlayerDevice is subscribed
+//
+bool CPlayerDevice::IsSubscribed()
+{
+    m_world->LockShmem();
+    bool subscribed = m_info_buffer[INFO_SUBSCRIBE_FLAG];
+    m_world->UnlockShmem();
+
+    return subscribed;
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+// Write to the data buffer
+//
+size_t CPlayerDevice::PutData(void *data, size_t len)
+{
+    if (len > m_data_len)
+        MSG2("data len (%d) > buffer len (%d)", (int) len, (int) m_data_len);
+    ASSERT(len <= m_data_len);
+
+    m_world->LockShmem();
+
+    // Copy the data
+    //
+    memcpy(m_data_buffer, data, len);
+
+    // Set data flag to indicate data is available
+    //
+    m_info_buffer[INFO_DATA_FLAG] = 1;
+    
+    m_world->UnlockShmem();
+    return len;
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+// Read from the command buffer
+//
+size_t CPlayerDevice::GetCommand(void *data, size_t len)
+{   
+    if (len > m_command_len)
+        MSG2("command len (%d) > buffer len (%d)", (int) len, (int) m_command_len);
+    ASSERT(len <= m_command_len);
+    
+    m_world->LockShmem();
+
+    // See if there is any command info available
+    //
+    if (m_info_buffer[INFO_COMMAND_FLAG] != 0)
+    {
+        // Copy the command
+        //
+        memcpy(data, m_command_buffer, len);
+    }
+    else
+    {
+        // Reset the length to indicate that there is no new data
+        //
+        len = 0;
+    }
+    
+    m_world->UnlockShmem();
+    return len;
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+// Read from the configuration buffer
+//
+size_t CPlayerDevice::GetConfig(void *data, size_t len)
+{
+    if (len > m_config_len)
+        MSG2("config len (%d) > buffer len (%d)", (int) len, (int) m_config_len);
+    ASSERT(len <= m_config_len);
+    
+    m_world->LockShmem();
+
+    // See if there is any configuration info available
+    //
+    if (m_info_buffer[INFO_CONFIG_FLAG] != 0)
+    {
+        // Copy the configuration
+        //
+        memcpy(data, m_config_buffer, len);
+
+        // Reset the config flag to indicate data has been read
+        //
+        m_info_buffer[INFO_CONFIG_FLAG] = 0;
+    }
+    else
+    {
+        // Reset the length to indicate that there is no new data
+        //
+        len = 0;
+    }
+    
+    m_world->UnlockShmem();
+    return len;
+}
+
