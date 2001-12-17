9b15458c60fbf6e02cfdd30daf597eda67822f44
ahoward <ahoward@55cc97c4-4627-0410-81d5-de40420c0091>
2001-12-17 18:32:05 +0000

    Fixed matrix undraw bugs

--- include/matrix.hh
+++ include/matrix.hh
@@ -1,7 +1,7 @@
 // ==================================================================
 // Filename:	CMatrix.h
 //
-// $Id: matrix.hh,v 1.4 2001-09-28 21:55:42 gerkey Exp $
+// $Id: matrix.hh,v 1.5 2001-12-17 18:32:05 ahoward Exp $
 // RTV
 // ==================================================================
 
@@ -20,14 +20,16 @@ enum MatrixMode { mode_set, mode_unset };
 
 class CMatrix
 {
-public:
+  public:
   int	width;
   int	height;
-  
+
+  private:
   CEntity***  data;
-  unsigned char* current_slot;
+  unsigned char* used_slots;
   unsigned char* available_slots;
-  
+
+  public:
   char*	win_title;
   
   int initial_buf_size;
@@ -36,6 +38,7 @@ public:
   
   MatrixMode mode;
   void PrintCell( int cell );
+  void CheckCell( int cell );
   
   //CMatrix();
   CMatrix(int w,int h);
@@ -49,43 +52,43 @@ public:
 
   // get a pixel color by its x,y coordinate
   inline CEntity** get_cell(int x, int y)
-  { 
-    if (x<0 || x>=width || y<0 || y>=height) 
-    {
-      //fputs("Stage: WARNING: CEntity::get_cell(int,int) out-of-bounds\n",
-            //stderr);
-      return 0;
-    }
+    { 
+      if (x<0 || x>=width || y<0 || y>=height) 
+      {
+        //fputs("Stage: WARNING: CEntity::get_cell(int,int) out-of-bounds\n",
+        //stderr);
+        return 0;
+      }
       
-    return data[x+(y*width)]; 
-  }
+      return data[x+(y*width)]; 
+    }
   
   // get a pixel color by its position in the array
   inline CEntity** get_cell( int i)
-  { 
-    if( i<0 || i > width*height ) 
-    {
-      //fputs("Stage: WARNING: CEntity::get_cell(int) out-of-bounds\n",stderr);
-      return 0;
+    { 
+      if( i<0 || i > width*height ) 
+      {
+        //fputs("Stage: WARNING: CEntity::get_cell(int) out-of-bounds\n",stderr);
+        return 0;
+      }
+      return data[i]; 
     }
-    return data[i]; 
-  }
   
   // is there an object of this type here?
   inline bool is_type( int x, int y, StageType type )
-  { 
-    //if( i<0 || i > width*height ) return 0;
+    { 
+      //if( i<0 || i > width*height ) return 0;
     
-    CEntity** cell = data[x+(y*width)];
+      CEntity** cell = data[x+(y*width)];
     
-    while( *cell )
+      while( *cell )
       {
-	if( (*cell)->m_stage_type == type ) return true;
-	cell++;
+        if( (*cell)->m_stage_type == type ) return true;
+        cell++;
       }
     
-    return false;
-  }
+      return false;
+    }
   
 
   inline void set_cell(int x, int y, CEntity* ent );
@@ -97,10 +100,8 @@ public:
   void	copy_from(CMatrix* img);
   
   void	draw_circle(int x,int y,int r,CEntity* ent);
-  void	draw_line(int x1,int y1,int x2,int y2,CEntity* ent);
-  
-  // new 1 Dec 2000 - RTV
   void	draw_rect( const Rect& t, CEntity* ent );
+  void	draw_line(int x1,int y1,int x2,int y2,CEntity* ent);
   
   CEntity** line_detect(int x1,int y1,int x2,int y2);
   CEntity** rect_detect( const Rect& r);
