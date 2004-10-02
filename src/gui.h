a97b725b9ca4240e4eaa3acb89c5fd7f53c4feec
rtv <rtv@55cc97c4-4627-0410-81d5-de40420c0091>
2004-10-02 02:19:33 +0000

    removing some dead files

--- src/gui.h
+++ src/gui.h
@@ -59,3 +59,16 @@
 #define STG_LASER_GEOM_COLOR "blue"
 #define STG_LASER_CFG_COLOR "light steel blue"
 #define STG_LASER_BRIGHT_COLOR "blue"
+
+  // GUI colors   
+#define STG_GRID_MAJOR_COLOR "gray85"
+#define STG_GRID_MINOR_COLOR "gray95"
+#define STG_MATRIX_COLOR "dark green"
+#define STG_BACKGROUND_COLOR "ivory"
+  
+  // model color defaults
+#define STG_GENERIC_COLOR "black"
+#define STG_POSITION_COLOR "red"
+#define STG_WALL_COLOR "dark blue"
+#define STG_SONAR_COLOR "gray70"
+
