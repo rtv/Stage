5e42038899aa42e5b52e9662efef6e7571b7e715
rtv <rtv@55cc97c4-4627-0410-81d5-de40420c0091>
2004-10-11 04:59:02 +0000

    adding doc stuff

---
+++ docsrc/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+OUTPUT=header.html
+PAGEDIR=html
+
+cat header_top.src > $OUTPUT
+
+for PAGE in `ls $PAGEDIR/group__model*.html` ; do
+
+   PAGENAME=${PAGE#$PAGEDIR/}
+   STRIPEND=${PAGE%.html}
+   STRIPPED=${STRIPEND#$PAGEDIR/group__model__}
+   
+   echo "Adding link for " $STRIPPED
+   echo "<a href=\"" $PAGENAME "\">" $STRIPPED "</a><br>" >> $OUTPUT
+done
+
+cat header_bottom.src >> $OUTPUT
