8c12c789e4a8a8449d1203d1f7be65e246d68d1d
rtv <rtv@55cc97c4-4627-0410-81d5-de40420c0091>
2004-10-29 01:24:44 +0000

    fixed collision bug

--- src/model_pose.c
+++ src/model_pose.c
@@ -7,7 +7,7 @@
 // CVS info:
 //  $Source: /home/tcollett/stagecvs/playerstage-cvs/code/stage/src/model_pose.c,v $
 //  $Author: rtv $
-//  $Revision: 1.32 $
+//  $Revision: 1.33 $
 //
 ///////////////////////////////////////////////////////////////////////////
 
@@ -86,15 +86,14 @@ stg_model_t* stg_model_test_collision_at_pose( stg_model_t* mod,
 
       stg_model_t* hitmod = itl_first_matching( itl, lines_raytrace_match, mod );
       
+      itl_destroy( itl );
+
       if( hitmod )
 	{
 	  if( hitx ) *hitx = itl->x; // report them
 	  if( hity ) *hity = itl->y;	  
+	  return hitmod; // we hit this object! stop raytracing
 	}
-
-      itl_destroy( itl );
-
-      return hitmod; // we hit this object! stop raytracing
     }
 
   return NULL;  // done 
