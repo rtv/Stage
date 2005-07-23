061c43cfdf9651e628e9a317dddf1549310767b3
adam_lein <adam_lein@55cc97c4-4627-0410-81d5-de40420c0091>
2005-07-23 07:35:47 +0000

    Outputs pose changes on all models.

--- src/referees/myref.cc
+++ src/referees/myref.cc
@@ -4,14 +4,29 @@
 class MyRef : public ZooReferee {
 public:
 	MyRef(ConfigFile *, int, ZooDriver *);
+private:
+	static int pose_cb(stg_model_t *, const char *, double *, size_t);
+	ZooDriver *zoo;
 };
 
 MyRef::MyRef( ConfigFile *cf, int section, ZooDriver *_zoo )
 	: ZooReferee(cf, section, _zoo)
 {
-	printf("myref_message = %s\n", cf->ReadString(section, "myref_message", ""));
-	//stg_err(cf->ReadString(section, "myref_message", ""));
-	fputc('\n', stderr);
+	printf("myref_message = %s\n",
+		cf->ReadString(section, "myref_message", ""));
+	zoo = _zoo;
+
+	stg_world_add_property_callback(StgDriver::world, "pose",
+		(stg_property_callback_t)pose_cb, NULL);
+}
+
+int
+MyRef::pose_cb( stg_model_t *mod, const char *prop,
+                    double *data, size_t len )
+{
+	printf("Zoo: Model %s now has pose [%.2f, %.2f, %.3f]\n",
+		mod->token, data[0], data[1], data[2]);
+	return 0;
 }
 
 extern "C" {
