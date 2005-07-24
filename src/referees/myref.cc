c2246059a6afe62a6bc8034bf52c5558d1d5e9cd
adam_lein <adam_lein@55cc97c4-4627-0410-81d5-de40420c0091>
2005-07-24 08:36:59 +0000

    Inline documentation and GPL info.

--- src/referees/myref.cc
+++ src/referees/myref.cc
@@ -1,6 +1,30 @@
+/*
+ *  A sample referee for use with Zoo.
+ *  Copyright (C) 2005 Adam Lein
+ *                      
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ *  USA
+ *
+ */
+
 #include <zoo_driver.h>
 #include <stdio.h>
 
+/* simply subclass the referee.  I'm going to do some special things in the
+ * constructor, and it feels nice to have the pose callback be a static class
+ * member. */
 class MyRef : public ZooReferee {
 public:
 	MyRef(ConfigFile *, int, ZooDriver *);
@@ -9,6 +33,11 @@ private:
 	ZooDriver *zoo;
 };
 
+/**
+ * MyRef::MyRef:  Read a line from the ConfigFile to see that it works, and
+ * print it out.  Then add the pose_cb callback to the pose property of every
+ * model.
+ */
 MyRef::MyRef( ConfigFile *cf, int section, ZooDriver *_zoo )
 	: ZooReferee(cf, section, _zoo)
 {
@@ -20,6 +49,9 @@ MyRef::MyRef( ConfigFile *cf, int section, ZooDriver *_zoo )
 		(stg_property_callback_t)pose_cb, NULL);
 }
 
+/**
+ * MyRef::pose_cb: The pose callback.  Just print the new value.
+ */
 int
 MyRef::pose_cb( stg_model_t *mod, const char *prop,
                     double *data, size_t len )
@@ -29,6 +61,10 @@ MyRef::pose_cb( stg_model_t *mod, const char *prop,
 	return 0;
 }
 
+/** 
+ * This function will be loaded and called by the ZooDriver when the time is
+ * ripe.  It should return a pointer to a ZooReferee object.
+ */
 extern "C" {
 	void *
 	zooref_create( ConfigFile *cf, int section, ZooDriver *_zoo )
