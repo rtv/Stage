fb72bee61d005199bcfc7321a3f676517f2e677a
adam_lein <adam_lein@55cc97c4-4627-0410-81d5-de40420c0091>
2005-08-10 08:30:13 +0000

    Uses SetScore to set a toy score for display.  Display callbacks are set manually, using stg_model_add_property_callback.  I'd prefer SetScoreDrawCB, but C++ name mangling is making my life difficult.  I'd even prefer stg_world_add_property_callback, and have no excuse here.

--- src/referees/myref.cc
+++ src/referees/myref.cc
@@ -21,6 +21,11 @@
 
 #include <zoo_driver.h>
 #include <stdio.h>
+#include <gtk/gtk.h>
+#include <math.h>
+#include <gui.h>
+
+#define MYREF_SCORE_FIG_NAME "myref_score_fig"
 
 /* simply subclass the referee.  I'm going to do some special things in the
  * constructor, and it feels nice to have the pose callback be a static class
@@ -30,8 +35,12 @@ public:
 	MyRef(ConfigFile *, int, ZooDriver *);
 private:
 	static int pose_cb(stg_model_t *, const char *, double *, size_t);
-	ZooDriver *zoo;
+	static int score_draw_cb(stg_model_t *, const char *, const double *);
+	static ZooDriver *zoo;
 };
+
+ZooDriver *MyRef::zoo = NULL; /* just initially; we'll set it to something
+                               * appropriate in the constructor. */
 
 /**
  * MyRef::MyRef:  Read a line from the ConfigFile to see that it works, and
@@ -56,25 +65,84 @@ MyRef::MyRef( ConfigFile *cf, int section, ZooDriver *_zoo )
 	 * doens't exactly match that of a stg_property_callback_t. */
 	stg_world_add_property_callback(StgDriver::world, "pose",
 		(stg_property_callback_t)pose_cb, NULL);
+
+	/* set the default score-drawing callback.  Maybe this should be done in
+	 * Prepare? */
+/* FIXME: goddamned name mangling... */
+#if 0
+	zoo->SetScoreDrawCB((zooref_score_draw_t)score_draw_cb, NULL);
+#else
+	int x=0;
+	for (int i=0; i < zoo->GetModelCount(); ++i) {
+		stg_model_t *mod = zoo->GetModelByIndex(i);
+		if (!mod) continue;
+		stg_model_set_property(mod, ZOO_SCORE_PROPERTY_NAME, &x, sizeof(int));
+		stg_model_add_property_toggles(mod, ZOO_SCORE_PROPERTY_NAME,
+			(stg_property_callback_t)score_draw_cb, (char *)"foo",
+			NULL, NULL, ZOO_SCORE_LABEL, 0);
+	}
+#endif
 }
 
 /**
- * MyRef::pose_cb: The pose callback.  Just print the new value.  Note that
- * the formal parameter list doesn't exactly match stg_property_callback_t
- * in that the user_data pointer is absent.  Doesn't matter, as long as the
- * formal parameter list is a prefix (w.r.t. types) of that of
- * stg_property_callback_t.
+ * MyRef::pose_cb: The pose callback.  This sets the robot's score, in this
+ * case, calculated in a toy fashion as an example.
  */
 int
 MyRef::pose_cb( stg_model_t *mod, const char *prop,
                     double *data, size_t len )
 {
-	/* print out the name of the model that moved, and the new pose as [x, y,
-	 * theta] */
-	printf("Zoo: Model %s now has pose [%.2f, %.2f, %.3f]\n",
-		mod->token, data[0], data[1], data[2]);
+	/* set the score of the robot to be the distance from the absolute
+	 * origin. */
+	double s = sqrt(data[0]*data[0] + data[1]*data[1]);
+	double olds;
+
+	zoo->SetScore(mod->token, &s, sizeof(double));
 
 	/* continue marshalling callbacks. */
+	return 0;
+}
+
+/**
+ * MyRef::score_draw_cb: Draw the score on the GUI.  Note the other
+ * parameters aren't there because we don't need them.  That's okay, because
+ * when we attach the callback, we can just cast the function pointer.
+ *
+ * Note: the framework for this subroutine was taken from model_position.c,
+ * the position_render_data() function, and model_laser.c, the
+ * laser_render_data() function.  The latter was for figuring out where to
+ * draw the data.
+ */
+int
+MyRef::score_draw_cb( stg_model_t *mod, const char *prop, const double *score )
+{
+	stg_rtk_fig_t *fig = stg_model_get_fig(mod, MYREF_SCORE_FIG_NAME);
+
+	if (!fig) {
+		fig = stg_model_fig_create(mod, MYREF_SCORE_FIG_NAME, NULL,
+			STG_LAYER_USER);
+		stg_color_t *color = (stg_color_t *)stg_model_get_property_fixed(mod,
+			"color", sizeof(stg_color_t));
+		stg_rtk_fig_color_rgb32(fig, *color);
+	}
+
+	/* if the robot is uncontrolled, don't print the score */
+	stg_rtk_fig_clear(fig);
+	if (!mod->subs) return 0;
+
+	/* what to draw */
+	char buf[256];
+	snprintf(buf, sizeof(buf), "<%.2f>", *score);
+
+	/* where to print it */
+	stg_pose_t pose;
+	stg_model_get_global_pose(mod, &pose);
+	stg_rtk_fig_origin(fig, pose.x, pose.y, 0);
+
+	/* draw it, over and to the left of the position model */
+	stg_rtk_fig_text(fig, -0.75, -0.75, 0.0, buf);
+
+	/* keep marshalling callbacks */
 	return 0;
 }
 
