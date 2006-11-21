b8b023dd07a5ffdc6a15a70bf6e4fbf18b0dee98
rtv <rtv@55cc97c4-4627-0410-81d5-de40420c0091>
2006-11-21 01:48:47 +0000

    fixed bug in handling laser_bright setting. Thanks to mzuluaga.

--- src/model_laser.c
+++ src/model_laser.c
@@ -7,7 +7,7 @@
 // CVS info:
 //  $Source: /home/tcollett/stagecvs/playerstage-cvs/code/stage/src/model_laser.c,v $
 //  $Author: rtv $
-//  $Revision: 1.89 $
+//  $Revision: 1.90 $
 //
 ///////////////////////////////////////////////////////////////////////////
 
@@ -286,7 +286,7 @@ int laser_update( stg_model_t* mod )
       if( hitmod )
 	{
 	  scan[t].reflectance = 
-	    (mod->laser_return >= LaserBright) ? 1.0 : 0.0;
+	    (hitmod->laser_return >= LaserBright) ? 1.0 : 0.0;
 	}
       else
 	scan[t].reflectance = 0.0;
