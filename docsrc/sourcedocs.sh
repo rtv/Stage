731b4376259df9b0b06bedde6b38e4c654b5b6a6
jeremy_asher <jeremy_asher@55cc97c4-4627-0410-81d5-de40420c0091>
2008-07-11 21:33:17 +0000

    Fixed docs auto-building, AUTHORS.txt

--- docsrc/sourcedocs.sh
+++ docsrc/sourcedocs.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 echo "/** \mainpage The Stage Robot Simulator"
-./Markdown.pl ../README
+./Markdown.pl ../README.txt
 echo "**/"
 
 echo "/** \page install Installation"
-./Markdown.pl ../INSTALL
+./Markdown.pl ../INSTALL.txt
 echo "**/"
 
 echo "/** \page release Release Notes"
-./Markdown.pl ../RELEASE
+./Markdown.pl ../RELEASE.txt
+echo "**/"
+
+echo "/** \page authors Authors"
+./Markdown.pl ../AUTHORS.txt
 echo "**/"
