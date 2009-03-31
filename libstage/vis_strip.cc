d91f1507e019b6f3b33a877a5f7bf985b7f054a0
rtv <rtv@55cc97c4-4627-0410-81d5-de40420c0091>
2009-03-31 22:46:27 +0000

    added missing file

---
+++ libstage/vis_strip.cc
@@ -0,0 +1,63 @@
+/*  Strip plot visualizer
+ *  Richard Vaughan 30 March 2009
+ */
+
+#include "stage.hh"
+#include "canvas.hh"
+using namespace Stg;
+
+StripPlotVis::StripPlotVis( float x, float y, float w, float h, 
+									 size_t len, 
+									 stg_color_t fgcolor, stg_color_t bgcolor,
+									 const char* name, const char* wfname )
+  : Visualizer( name, wfname ),
+	 data( new float[len] ),
+	 len(len),
+	 count(0),
+	 x(x),
+	 y(y),
+	 w(w),
+	 h(h),
+	 min(1e32),
+	 max(-1e32),
+	 fgcolor(fgcolor),
+	 bgcolor(bgcolor)
+{
+  // zero the data
+  memset( data, 0, len * sizeof(float ) );
+}
+
+StripPlotVis::~StripPlotVis()
+{
+  if( data )
+	 free( data );
+}
+
+void StripPlotVis::Visualize( Model* mod, Camera* cam )
+{  
+  Canvas* canvas = dynamic_cast<WorldGui*>(mod->GetWorld())->GetCanvas();
+
+  if( ! canvas->selected( mod ) ) // == canvas->SelectedVisualizeAll() )
+	 return;
+
+  canvas->EnterScreenCS();
+  
+  mod->PushColor( bgcolor );
+  glRectf( x,y,w,h );
+  mod->PopColor();
+  
+  mod->PushColor( fgcolor );
+  Gl::draw_array( x,y,w,h,data,len,count%len,min,max );
+  mod->PopColor();
+  
+  canvas->LeaveScreenCS();
+}
+
+void StripPlotVis::AppendValue( float value )
+{
+  data[count%len] = value;
+  count++;
+  
+  min = MIN( value, min );
+  max = MAX( value, max );
+}
