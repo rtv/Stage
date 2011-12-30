1863a7a82aae3fc8817401bbe4e38434e085d11f
Richard Vaughan <vaughan@sfu.ca>
2011-12-29 16:46:03 -0800

    docs

--- libstage/main.cc
+++ libstage/main.cc
@@ -1,7 +1,27 @@
 /**
   \defgroup stage The Stage standalone robot simulator.
 
-  Here is where I describe the Stage standalone program.
+  USAGE:  stage [options] <worldfile1> [worldfile2 ... ]
+
+  Available [options] are:
+
+    --clock        : print simulation time peridically on standard output
+
+    -c             : equivalent to --clock
+
+    --gui          : run without a GUI
+
+    -g             : equivalent to --gui
+
+    --help         : print this message
+
+    --args \"str\"   : define an argument string to be passed to all controllers
+
+    -a \"str\"       : equivalent to --args "str"
+
+    -h             : equivalent to --help"
+
+    -?             : equivalent to --help
  */
 
 #include <getopt.h>
